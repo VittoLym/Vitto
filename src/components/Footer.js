f27ff0c19bcab62559bf2a08512c2e5b97b0b66a
vittolym <lexymdz@gmail.com>
2025-11-13 15:35:14 -0300

    footer init

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import { MailchimpForm } from "./MailchimpForm";
 import logo from "../assets/img/logo.svg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
-import navIcon2 from "../assets/img/nav-icon2.svg";
+import navIcon2 from '../assets/img/nav-gmail.svg';
 import navIcon3 from "../assets/img/nav-icon3.svg";
 
 export const Footer = () => {
@@ -16,9 +16,9 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+              <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/devvitto/"><img src={navIcon1} alt="LinkedIn" /></a>
+              <a target="_blank" rel="noreferrer" href="mailto:vitto.jsx@gmail.com?subject=Contacto%20desde%20tu%20portfolio&body=Hola%20Vitto,"><img src={navIcon2} alt="Gmail" /></a>
+              <a target="_blank" rel="noreferrer" href="https://www.instagram.com/devitto.jsx/"><img src={navIcon3} alt="Instagram" /></a>
             </div>
             <p>Copyright 2025. All Rights Reserved</p>
           </Col>
