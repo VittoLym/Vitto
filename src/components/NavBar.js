822157c86cdd49b0b5b8d6e9a92042034a10f25d
vittolym <lexymdz@gmail.com>
2026-05-27 21:35:19 -0300

    new logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -41,7 +41,11 @@ export const NavBar = () => {
       <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
         <Container>
           <Navbar.Brand href="/">
-            <img src={logo} alt="Vitto" />
+            <img 
+              src={logo} 
+              alt="Vitto"
+              style={{ width: "auto", height: "45px", display: "block" }}
+            />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
